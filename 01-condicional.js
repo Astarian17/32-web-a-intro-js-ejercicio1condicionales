670779b9e93dbf61794cc09e5b994e0e5d9ec019
Mauricio Bracho <maubracho@gmail.com>
2024-02-13 20:18:07 -0700

    agregado el html de ejercicio

--- 01-condicional.js
+++ 01-condicional.js
@@ -1,10 +1,10 @@
 let respuesta
 prompt ("eres hermoso?")
 console.log("eres hermoso?")
-if (respuesta=si)
+if (respuesta=="si")
 {console.log("es totalmente cierto")
 }
-else (respuesta=no)
+else (respuesta=="no")
 {
 console.log("no te creo")
 }
