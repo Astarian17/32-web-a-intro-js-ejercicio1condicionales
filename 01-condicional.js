a73e6416b11e9fd3f79712f9219505cd4e2a4698
Mauricio Bracho <maubracho@gmail.com>
2024-02-13 19:59:42 -0700

    creacion del ejercicio 1 de condicionales

---
+++ 01-condicional.js
@@ -0,0 +1,10 @@
+let respuesta
+prompt ("eres hermoso?")
+console.log("eres hermoso?")
+if (respuesta=si)
+{console.log("es totalmente cierto")
+}
+else (respuesta=no)
+{
+console.log("no te creo")
+}
